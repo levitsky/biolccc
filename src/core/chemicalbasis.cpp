2ad334dbad28b5ff652afef36c9ee642a4d62ddb
Anton Goloborodko <goloborodko.anton@gmail.com>
2010-08-17 16:16:59 +0400

    chemicalbasis.cpp and chemicalgroup.cpp are formatted with the Allman style.

--- src/core/chemicalbasis.cpp
+++ src/core/chemicalbasis.cpp
@@ -1,162 +1,164 @@
 #include "chemicalbasis.h"
 
-namespace BioLCCC {
+namespace BioLCCC
+{
 ChemicalBasisException::ChemicalBasisException(std::string message):
-    BioLCCCException(message) {};
+        BioLCCCException(message) {};
 
-ChemicalBasis::ChemicalBasis() {
+ChemicalBasis::ChemicalBasis()
+{
     setModel(COIL_BOLTZMANN);
-    
+
     //adding standard aminoacids, masses set to zero.
     addChemicalGroup(ChemicalGroup ("Alanine",
-                                "A",
-                                1.1425,
-                                71.0788,
-                                71.03711));
+                                    "A",
+                                    1.1425,
+                                    71.0788,
+                                    71.03711));
     addChemicalGroup(ChemicalGroup ("Cysteine",
-                                "C",
-                                1.2955,
-                                103.1388,
-                                103.00919));
+                                    "C",
+                                    1.2955,
+                                    103.1388,
+                                    103.00919));
     addChemicalGroup(ChemicalGroup ("Carboxyamidomethylated cysteine",
-                                "camC",
-                                0.77,
-                                160.1901,
-                                160.03065));
+                                    "camC",
+                                    0.77,
+                                    160.1901,
+                                    160.03065));
     addChemicalGroup(ChemicalGroup ("Aspartic_acid",
-                                "D",
-                                0.7805,
-                                115.0886,
-                                115.02694));
+                                    "D",
+                                    0.7805,
+                                    115.0886,
+                                    115.02694));
     addChemicalGroup(ChemicalGroup ("Glutamic_acid",
-                                "E",
-                                0.9835,
-                                129.1155,
-                                129.04259));
+                                    "E",
+                                    0.9835,
+                                    129.1155,
+                                    129.04259));
     addChemicalGroup(ChemicalGroup ("Phenylalanine",
-                                "F",
-                                2.3185,
-                                147.1766,
-                                147.06841));
+                                    "F",
+                                    2.3185,
+                                    147.1766,
+                                    147.06841));
     addChemicalGroup(ChemicalGroup ("Glycine",
-                                "G",
-                                0.6555,
-                                57.0519,
-                                57.02146));
+                                    "G",
+                                    0.6555,
+                                    57.0519,
+                                    57.02146));
     addChemicalGroup(ChemicalGroup ("Histidine",
-                                "H",
-                                0.3855,
-                                137.1411,
-                                137.05891));
+                                    "H",
+                                    0.3855,
+                                    137.1411,
+                                    137.05891));
     addChemicalGroup(ChemicalGroup ("Isoleucine",
-                                "I",
-                                2.1555,
-                                113.1594,
-                                113.08406));
+                                    "I",
+                                    2.1555,
+                                    113.1594,
+                                    113.08406));
     addChemicalGroup(ChemicalGroup ("Lysine",
-                                "K",
-                                0.2655,
-                                128.1741,
-                                128.09496));
+                                    "K",
+                                    0.2655,
+                                    128.1741,
+                                    128.09496));
     addChemicalGroup(ChemicalGroup ("Leucine",
-                                "L",
-                                2.2975,
-                                113.1594,
-                                113.08406));
+                                    "L",
+                                    2.2975,
+                                    113.1594,
+                                    113.08406));
     addChemicalGroup(ChemicalGroup ("Methionine",
-                                "M",
-                                1.8215,
-                                131.1926,
-                                131.04049));
+                                    "M",
+                                    1.8215,
+                                    131.1926,
+                                    131.04049));
     addChemicalGroup(ChemicalGroup ("Oxidated methionine",
-                                "oxM",
-                                1.8215,
-                                131.1926 + 15.9994,
-                                131.04049 + 15.994915));
+                                    "oxM",
+                                    1.8215,
+                                    131.1926 + 15.9994,
+                                    131.04049 + 15.994915));
     addChemicalGroup(ChemicalGroup ("Asparagine",
-                                "N",
-                                0.6135,
-                                114.1038,
-                                114.04293));
+                                    "N",
+                                    0.6135,
+                                    114.1038,
+                                    114.04293));
     addChemicalGroup(ChemicalGroup ("Proline",
-                                "P",
-                                1.1425,
-                                97.1167,
-                                97.05276));
+                                    "P",
+                                    1.1425,
+                                    97.1167,
+                                    97.05276));
     addChemicalGroup(ChemicalGroup ("Glutamine",
-                                "Q",
-                                0.7455,
-                                128.1307,
-                                128.05858));
+                                    "Q",
+                                    0.7455,
+                                    128.1307,
+                                    128.05858));
     addChemicalGroup(ChemicalGroup ("Arginine",
-                                "R",
-                                0.5155,
-                                156.1875,
-                                156.10111));
+                                    "R",
+                                    0.5155,
+                                    156.1875,
+                                    156.10111));
     addChemicalGroup(ChemicalGroup ("Serine",
-                                "S",
-                                0.6975,
-                                87.0782,
-                                87.03203));
+                                    "S",
+                                    0.6975,
+                                    87.0782,
+                                    87.03203));
     addChemicalGroup(ChemicalGroup ("Phosphorylated serine",
-                                "pS",
-                                0.45,
-                                167.0581,
-                                166.99836));
+                                    "pS",
+                                    0.45,
+                                    167.0581,
+                                    166.99836));
     addChemicalGroup(ChemicalGroup ("Threonine",
-                                "T",
-                                0.8755,
-                                101.1051,
-                                101.04768));
+                                    "T",
+                                    0.8755,
+                                    101.1051,
+                                    101.04768));
     addChemicalGroup(ChemicalGroup ("Phosphorylated threonine",
-                                "pT",
-                                0.74,
-                                181.085,
-                                181.01401));
+                                    "pT",
+                                    0.74,
+                                    181.085,
+                                    181.01401));
     addChemicalGroup(ChemicalGroup ("Valine",
-                                "V",
-                                1.7505,
-                                99.1326,
-                                99.06841));
+                                    "V",
+                                    1.7505,
+                                    99.1326,
+                                    99.06841));
     addChemicalGroup(ChemicalGroup ("Tryptophan",
-                                "W",
-                                2.4355,
-                                186.2132,
-                                186.07931));
+                                    "W",
+                                    2.4355,
+                                    186.2132,
+                                    186.07931));
     addChemicalGroup(ChemicalGroup ("Tyrosine",
-                                "Y",
-                                1.6855,
-                                163.176,
-                                163.06333));
+                                    "Y",
+                                    1.6855,
+                                    163.176,
+                                    163.06333));
     addChemicalGroup(ChemicalGroup ("Phosphorylated tyrosine",
-                                "pY",
-                                1.32,
-                                243.1559,
-                                243.02966));
-                                    
+                                    "pY",
+                                    1.32,
+                                    243.1559,
+                                    243.02966));
+
     // Adding standard terminal groups.
     addChemicalGroup(ChemicalGroup("N-terminal hydrogen",
-                            "H-",
-                            -1.69,
-                            1.0079,
-                            1.00782));
+                                   "H-",
+                                   -1.69,
+                                   1.0079,
+                                   1.00782));
     addChemicalGroup(ChemicalGroup("N-terminal acetyl",
-                            "Ac-",
-                            0.0,
-                            43.0452,
-                            43.01839));
+                                   "Ac-",
+                                   0.0,
+                                   43.0452,
+                                   43.01839));
     addChemicalGroup(ChemicalGroup("C-terminal carboxyl",
-                            "-COOH",
-                            -0.03,
-                            17.0073,
-                            17.00274));
+                                   "-COOH",
+                                   -0.03,
+                                   17.0073,
+                                   17.00274));
     addChemicalGroup(ChemicalGroup("C-terminal amide",
-                            "-NH2",
-                            0.0,
-                            16.0226,
-                            16.01872));
-                                    
-    // setting standard second solvent bind energy 
+                                   "-NH2",
+                                   0.0,
+                                   16.0226,
+                                   16.01872));
+
+    // setting standard second solvent bind energy
     setSecondSolventBindEnergy(2.3979);
 
     // setting standard (BUT NOT CORRECT) value of the peptide segment length
@@ -169,43 +171,53 @@ ChemicalBasis::ChemicalBasis() {
     setPersistentLength(1);
 }
 
-const std::map<std::string,ChemicalGroup> & ChemicalBasis::chemicalGroups() const
+const std::map<std::string,ChemicalGroup> & 
+    ChemicalBasis::chemicalGroups() const
 {
     return mChemicalGroups;
 }
 
-const ChemicalGroup & ChemicalBasis::defaultNTerminus() const {
-    std::map<std::string, ChemicalGroup>::const_iterator NTerminusIterator = 
+const ChemicalGroup & ChemicalBasis::defaultNTerminus() const
+{
+    std::map<std::string, ChemicalGroup>::const_iterator NTerminusIterator =
         mChemicalGroups.find("H-");
-    if (NTerminusIterator == mChemicalGroups.end()) {
+    if (NTerminusIterator == mChemicalGroups.end())
+    {
         throw ChemicalBasisException("The default H- N-terminus not found.");
     }
     return NTerminusIterator->second;
 }
 
-const ChemicalGroup & ChemicalBasis::defaultCTerminus() const{
-    std::map<std::string, ChemicalGroup>::const_iterator CTerminusIterator = 
+const ChemicalGroup & ChemicalBasis::defaultCTerminus() const
+{
+    std::map<std::string, ChemicalGroup>::const_iterator CTerminusIterator =
         mChemicalGroups.find("-COOH");
-    if (CTerminusIterator == mChemicalGroups.end()) {
+    if (CTerminusIterator == mChemicalGroups.end())
+    {
         throw ChemicalBasisException("The default -COOH C-terminus not found.");
     }
     return CTerminusIterator->second;
 }
 
-double ChemicalBasis::secondSolventBindEnergy() const{
+double ChemicalBasis::secondSolventBindEnergy() const
+{
     return mSecondSolventBindEnergy;
 }
 
-void ChemicalBasis::setSecondSolventBindEnergy(double newEnergy){
+void ChemicalBasis::setSecondSolventBindEnergy(double newEnergy)
+{
     mSecondSolventBindEnergy = newEnergy;
 }
 
-double ChemicalBasis::segmentLength() const {
+double ChemicalBasis::segmentLength() const
+{
     return mSegmentLength;
 }
 
-void ChemicalBasis::setSegmentLength(double newSegmentLength) {
-    if (newSegmentLength <= 0.0) {
+void ChemicalBasis::setSegmentLength(double newSegmentLength)
+{
+    if (newSegmentLength <= 0.0)
+    {
         throw ChemicalBasisException(
             "The new length of a segment is not positive.");
     }
@@ -213,12 +225,15 @@ void ChemicalBasis::setSegmentLength(double newSegmentLength) {
     mSegmentLength = newSegmentLength;
 }
 
-int ChemicalBasis::persistentLength() const {
+int ChemicalBasis::persistentLength() const
+{
     return mPeristentLength;
 }
 
-void ChemicalBasis::setPersistentLength(int newPersistentLength) {
-    if (newPersistentLength <= 0) {
+void ChemicalBasis::setPersistentLength(int newPersistentLength)
+{
+    if (newPersistentLength <= 0)
+    {
         throw ChemicalBasisException(
             "The new persistent length is not positive.");
     }
@@ -226,12 +241,15 @@ void ChemicalBasis::setPersistentLength(int newPersistentLength) {
     mPeristentLength = newPersistentLength;
 }
 
-double ChemicalBasis::adsorbtionLayerWidth() const {
+double ChemicalBasis::adsorbtionLayerWidth() const
+{
     return mAdsorbtionLayerWidth;
 }
 
-void ChemicalBasis::setAdsorbtionLayerWidth(double newAdsorbtionLayerWidth) {
-    if (newAdsorbtionLayerWidth < 0.0) {
+void ChemicalBasis::setAdsorbtionLayerWidth(double newAdsorbtionLayerWidth)
+{
+    if (newAdsorbtionLayerWidth < 0.0)
+    {
         throw ChemicalBasisException(
             "The new adsorbtion layer width is negative.");
     }
@@ -239,38 +257,45 @@ void ChemicalBasis::setAdsorbtionLayerWidth(double newAdsorbtionLayerWidth) {
     mAdsorbtionLayerWidth = newAdsorbtionLayerWidth;
 }
 
-void ChemicalBasis::addChemicalGroup(ChemicalGroup newChemicalGroup){
+void ChemicalBasis::addChemicalGroup(ChemicalGroup newChemicalGroup)
+{
     mChemicalGroups[newChemicalGroup.label()] = newChemicalGroup;
 }
 
-void ChemicalBasis::removeChemicalGroup(std::string label){
-    if (mChemicalGroups.erase(label) == (std::size_t)0) {
+void ChemicalBasis::removeChemicalGroup(std::string label)
+{
+    if (mChemicalGroups.erase(label) == (std::size_t)0)
+    {
         throw ChemicalBasisException(
             "The chemical group " + label + " is not found.");
     }
 }
 
-void ChemicalBasis::clearChemicalGroups(){
+void ChemicalBasis::clearChemicalGroups()
+{
     mChemicalGroups.clear();
 }
 
-void ChemicalBasis::setChemicalGroupBindEnergy(std::string label, 
-    double newBindEnergy)
+void ChemicalBasis::setChemicalGroupBindEnergy(std::string label,
+        double newBindEnergy)
 {
-    std::map<std::string,ChemicalGroup>::iterator it = 
+    std::map<std::string,ChemicalGroup>::iterator it =
         mChemicalGroups.find(label);
-    if (it == mChemicalGroups.end()) {
+    if (it == mChemicalGroups.end())
+    {
         throw ChemicalBasisException(
             "The chemical group " + label + " is not found.");
     }
     it->second.setBindEnergy(newBindEnergy);
 }
 
-const ModelType ChemicalBasis::model() const{
+const ModelType ChemicalBasis::model() const
+{
     return mModel;
 }
 
-void ChemicalBasis::setModel(ModelType newModel){
+void ChemicalBasis::setModel(ModelType newModel)
+{
     mModel = newModel;
 }
 }
--- src/core/chemicalgroup.cpp
+++ src/core/chemicalgroup.cpp
@@ -1,13 +1,15 @@
 #include "chemicalgroup.h"
 
-namespace BioLCCC {
+namespace BioLCCC
+{
 
 ChemicalGroup::ChemicalGroup(std::string name,
-    std::string label,
-    double bindEnergy,
-    double averageMass,
-    double monoisotopicMass
-) {
+                             std::string label,
+                             double bindEnergy,
+                             double averageMass,
+                             double monoisotopicMass
+                            )
+{
     mName = name;
     mLabel = label;
     mAverageMass = averageMass;
@@ -15,39 +17,48 @@ ChemicalGroup::ChemicalGroup(std::string name,
     mBindEnergy = bindEnergy;
 }
 
-std::string ChemicalGroup::name() const{
+std::string ChemicalGroup::name() const
+{
     return mName;
 }
 
-std::string ChemicalGroup::label() const{
+std::string ChemicalGroup::label() const
+{
     return mLabel;
 }
 
-double ChemicalGroup::bindEnergy() const{
+double ChemicalGroup::bindEnergy() const
+{
     return mBindEnergy;
 }
 
-double ChemicalGroup::averageMass() const{
+double ChemicalGroup::averageMass() const
+{
     return mAverageMass;
 }
 
-double ChemicalGroup::monoisotopicMass() const{
+double ChemicalGroup::monoisotopicMass() const
+{
     return mMonoisotopicMass;
 }
 
-bool ChemicalGroup::isNTerminal() const{
+bool ChemicalGroup::isNTerminal() const
+{
     return (mLabel.find("-") == (size_t)(mLabel.size()-1));
 }
 
-bool ChemicalGroup::isCTerminal() const{
+bool ChemicalGroup::isCTerminal() const
+{
     return (mLabel.find("-") == (size_t)(0));
 }
 
-bool ChemicalGroup::isAminoAcid() const{
+bool ChemicalGroup::isAminoAcid() const
+{
     return (!(isCTerminal() || isNTerminal()));
 }
 
-void ChemicalGroup::setBindEnergy(double newBindEnergy){
+void ChemicalGroup::setBindEnergy(double newBindEnergy)
+{
     mBindEnergy = newBindEnergy;
 }
 }
