e5a4f171b1ce2981e5ba2b8fe24b3cc185086258
Anton Goloborodko <goloborodko.anton@gmail.com>
2010-08-17 16:06:30 +0400

    gradient.h and gradientpoint.h are formatted with the Allman style.

--- include/gradient.h
+++ include/gradient.h
@@ -2,41 +2,44 @@
 #define GRADIENT_H
 
 #include <vector>
-#include "gradientpoint.h" 
+#include "gradientpoint.h"
 #include "biolcccexception.h"
 
 //! The Gradient class encapsulates all properties of an elution gradient.
 
-namespace BioLCCC {
+namespace BioLCCC
+{
 
-class GradientException : public BioLCCCException {
-    public:
-        GradientException(std::string message);
+class GradientException : public BioLCCCException
+{
+public:
+    GradientException(std::string message);
 };
 
-class Gradient: public std::vector<GradientPoint> {
-    public:
-        /*!
-            Constructs an empty elution gradient.
-        */
-        Gradient();
+class Gradient: public std::vector<GradientPoint>
+{
+public:
+    /*!
+        Constructs an empty elution gradient.
+    */
+    Gradient();
 
-        /*!
-            Constructs linear elution gradient.
-        */
-        Gradient(double initialConcentrationB,
-                 double finalConcentrationB,
-                 double time);
+    /*!
+        Constructs linear elution gradient.
+    */
+    Gradient(double initialConcentrationB,
+             double finalConcentrationB,
+             double time);
 
-        /*!
-            Adds a new point to the gradient.
-        */
-        Gradient addPoint(GradientPoint iPoint);
+    /*!
+        Adds a new point to the gradient.
+    */
+    Gradient addPoint(GradientPoint iPoint);
 
-        /*!
-            Adds a new point to the gradient in more convenient way.
-        */
-        Gradient addPoint(double iTime, double iConcentrationB);
+    /*!
+        Adds a new point to the gradient in more convenient way.
+    */
+    Gradient addPoint(double iTime, double iConcentrationB);
 };
 
 }
--- include/gradientpoint.h
+++ include/gradientpoint.h
@@ -6,37 +6,40 @@
 //! The GradientPoint class encapsulates all properties of a point of a
 //gradient.
 
-namespace BioLCCC {
+namespace BioLCCC
+{
 
-class GradientPointException : public BioLCCCException {
-    public:
-        GradientPointException(std::string message);
+class GradientPointException : public BioLCCCException
+{
+public:
+    GradientPointException(std::string message);
 };
 
-class GradientPoint {
-    public:
-        /*!
-            Constructs a point of a gradient with the given time and the
-            concentration of the solvent B.
-        */
-        GradientPoint(double iTime = 0.0,
-                      double iConcentrationB = 0.0
-        );
+class GradientPoint
+{
+public:
+    /*!
+        Constructs a point of a gradient with the given time and the
+        concentration of the solvent B.
+    */
+    GradientPoint(double iTime = 0.0,
+                  double iConcentrationB = 0.0
+                 );
 
-        /*
-            Returns the time of the point in minutes.
-        */
-        double time() const;
+    /*
+        Returns the time of the point in minutes.
+    */
+    double time() const;
 
-        /*
-            Returns the corresponding concentration of the component B 
-            in percents.
-        */
-        double concentrationB() const;
+    /*
+        Returns the corresponding concentration of the component B
+        in percents.
+    */
+    double concentrationB() const;
 
-    private:
-        double mTime;
-        double mConcentrationB;
+private:
+    double mTime;
+    double mConcentrationB;
 };
 
 }
