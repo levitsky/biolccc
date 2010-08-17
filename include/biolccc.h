97eb719b261cfd1f493c688353267a8ef7f92edc
Anton Goloborodko <goloborodko.anton@gmail.com>
2010-08-17 16:07:04 +0400

    biolccc.h and biolcccexception.h are formatted with the Allman style.

--- include/biolccc.h
+++ include/biolccc.h
@@ -6,19 +6,21 @@
 #include "chemicalbasis.h"
 #include "chromoconditions.h"
 
-//! This is the main header for the whole BioLCCC library project. 
+//! This is the main header for the whole BioLCCC library project.
 
 /*!
     <b> Known issues </b>
-    - Due to the fundamental ambiguity you couldn't parse properly the 
+    - Due to the fundamental ambiguity you couldn't parse properly the
     sequence of amidated histidine (H-NH2).
 */
 
-namespace BioLCCC {
+namespace BioLCCC
+{
 
-class ParsingException : public BioLCCCException {
-    public:
-        ParsingException(std::string message);
+class ParsingException : public BioLCCCException
+{
+public:
+    ParsingException(std::string message);
 };
 
 static const ChromoConditions standardChromoConditions = ChromoConditions();
@@ -30,7 +32,7 @@ static const ChemicalBasis standardChemicalBasis = ChemicalBasis();
     of a peptide.
 */
 void parseSequence(
-    const std::string &source, 
+    const std::string &source,
     const ChemicalBasis &chemBasis,
     std::vector<ChemicalGroup> *parsedPeptideStructure,
     ChemicalGroup *NTerminus,
@@ -38,53 +40,53 @@ void parseSequence(
     std::vector<double> *peptideEnergyProfile);
 
 /*!
-    Calculates the retention time of a peptide with given sequence 
-    using given table of peptide chemicals and chromatographic 
+    Calculates the retention time of a peptide with given sequence
+    using given table of peptide chemicals and chromatographic
     conditions.
 */
 double calculateRT(const std::string &sequence,
-    const ChromoConditions & conditions = standardChromoConditions,
-    const ChemicalBasis & chemBasis = standardChemicalBasis,
-    const bool continueGradient = true);
+                   const ChromoConditions & conditions = standardChromoConditions,
+                   const ChemicalBasis & chemBasis = standardChemicalBasis,
+                   const bool continueGradient = true);
 
 /*!
-    Calculates the average (molar) mass of a peptide with given 
+    Calculates the average (molar) mass of a peptide with given
     sequence using given table of peptide chemicals.
 */
 double calculateAverageMass(const std::string &sequence,
-    const ChemicalBasis &chemBasis = standardChemicalBasis);
-                        
+                            const ChemicalBasis &chemBasis = standardChemicalBasis);
+
 /*!
-    Calculates the monoisotopic mass of a peptide with given sequence 
+    Calculates the monoisotopic mass of a peptide with given sequence
     using given table of peptide chemicals.
 */
 double calculateMonoisotopicMass(const std::string &sequence,
-    const ChemicalBasis &chemBasis = standardChemicalBasis);
+                                 const ChemicalBasis &chemBasis = standardChemicalBasis);
 
 /*!
     Calculates the distribution coefficient (Kd) of a peptide with
-    given sequence with the STANDARD BioLCCC model (for a coil using the 
-    Boltzmann equation) with given table of peptide chemicals, the name of 
-    the second solvent, its concentration, the size of adsorbent's pores, 
+    given sequence with the STANDARD BioLCCC model (for a coil using the
+    Boltzmann equation) with given table of peptide chemicals, the name of
+    the second solvent, its concentration, the size of adsorbent's pores,
     calibration parameter and temperature.
 */
 double calculateKd(const std::string &sequence,
-    const double secondSolventConcentration,
-    const ChemicalBasis &chemBasis = standardChemicalBasis,
-    const double columnPoreSize = 100.0,
-    const double calibrationParameter = 1.0,
-    const double temperature = 293.0);
+                   const double secondSolventConcentration,
+                   const ChemicalBasis &chemBasis = standardChemicalBasis,
+                   const double columnPoreSize = 100.0,
+                   const double calibrationParameter = 1.0,
+                   const double temperature = 293.0);
 
 /*!
-    Created as a transient solution for the fast calculation of RTBioLCCC 
+    Created as a transient solution for the fast calculation of RTBioLCCC
     and masses.
 */
 void calculatePeptideProperties(const std::string &sequence,
-    const ChromoConditions &conditions,
-    const ChemicalBasis &chemBasis,
-    double *RTBioLCCC,
-    double *averageMass,
-    double *monoisotopicMass);
+                                const ChromoConditions &conditions,
+                                const ChemicalBasis &chemBasis,
+                                double *RTBioLCCC,
+                                double *averageMass,
+                                double *monoisotopicMass);
 
 }
 #endif
