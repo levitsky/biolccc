4a1b83627e2f96d343ec7a9925462c5a57a4b3b9
Anton Goloborodko <goloborodko.anton@gmail.com>
2010-08-18 19:25:59 +0400

    Doxygen documentaion is updated

--- include/biolcccexception.h
+++ include/biolcccexception.h
@@ -6,11 +6,17 @@
 namespace BioLCCC
 {
 
+//! Base class for all BioLCCC exceptions. Can be used by itself.
 class BioLCCCException : public std::exception
 {
 public:
+    //! A default constructor.
     BioLCCCException(std::string message);
+
+    //! A default destructor.
     ~BioLCCCException() throw();
+
+    //! Returns a message.
     virtual const char* what() const throw();
 private:
     std::string mMessage;
