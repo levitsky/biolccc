86d24a48b36c22b02adf56e5d9ddde2bb220c8ce
Lev Levitsky <lev.levitsky@phystech.edu>
2021-05-21 16:12:46 +0300

    Address issue #1

--- src/core/auxiliary.cpp
+++ src/core/auxiliary.cpp
@@ -12,13 +12,13 @@ void fitSpline(const double *x, const double *y, const int n, double * y2)
     double * d1 = new double[n-1];
     c1[0] = 0.0;
     d1[0] = 0.0;
-    
+
     for (int i = 1; i < n - 1; i++)
     {
         a = x[i] - x[i-1];
         b = 2.0 * (x[i+1] - x[i-1]);
         c = x[i+1] - x[i];
-        d = 6.0 * ((y[i+1] - y[i]) / (x[i+1] - x[i]) 
+        d = 6.0 * ((y[i+1] - y[i]) / (x[i+1] - x[i])
                     - (y[i] - y[i-1]) / (x[i] - x[i-1]));
         c1[i] = c / (b - c1[i-1] * a);
         d1[i] = (d - d1[i-1] * a) / (b - c1[i-1] * a);
@@ -30,7 +30,8 @@ void fitSpline(const double *x, const double *y, const int n, double * y2)
         y2[i] = d1[i] - c1[i] * y2[i+1];
     }
 
-    delete[] c1, d1;
+    delete[] c1;
+    delete[] d1;
 }
 
 double calculateSpline(const double *x, const double *y, const double * y2,
@@ -38,7 +39,7 @@ double calculateSpline(const double *x, const double *y, const double * y2,
 {
     int j = 0;
     int j_up = n - 1;
-    while (j_up > j + 1) 
+    while (j_up > j + 1)
     {
         if ((x[j] <= x_in) && (x_in <= x[(j + j_up) / 2]))
         {
@@ -53,7 +54,7 @@ double calculateSpline(const double *x, const double *y, const double * y2,
     double dx = x[j+1] - x[j];
     double a = (x[j+1] - x_in) / dx;
     double b = (x_in - x[j]) / dx;
-    return a * y[j] + b * y[j+1] 
+    return a * y[j] + b * y[j+1]
         + ((a * a * a - a) * y2[j] + (b * b * b - b) * y2[j + 1])
           * (dx * dx) / 6.0;
 }
@@ -71,7 +72,7 @@ double linInterpolate(const double * x, const double * y, const int n,
     return y[n];
 }
 
-double polInterpolate(const double * x, const double * y, const int n, 
+double polInterpolate(const double * x, const double * y, const int n,
                       const double x_in)
 {
     double * p = new double[n];
@@ -92,12 +93,12 @@ double polInterpolate(const double * x, const double * y, const int n,
     return output;
 }
 
-double partPolInterpolate(const double * x, const double * y, 
+double partPolInterpolate(const double * x, const double * y,
     const int n, const int n_part, const double x_in)
 {
     int k = 0;
     int k_up = n - 1;
-    while (k_up > k + 1) 
+    while (k_up > k + 1)
     {
         if ((x[k] <= x_in) && (x_in <= x[(k + k_up) / 2]))
         {
@@ -139,7 +140,7 @@ void solveMatrixEquation(double * m, double * rhs, const int n)
         reduced[i] = false;
     }
 
-    for (int k=0; k<n; k++) 
+    for (int k=0; k<n; k++)
     {
         int pivot_i=0;
         int pivot_j=0;
@@ -177,7 +178,7 @@ void solveMatrixEquation(double * m, double * rhs, const int n)
             rhs[pivot_j] = temp;
         }
 
-        if (m[pivot_j * n + pivot_j] == 0.0) 
+        if (m[pivot_j * n + pivot_j] == 0.0)
         {
             throw("The matrix is singular.");
         }
@@ -194,7 +195,7 @@ void solveMatrixEquation(double * m, double * rhs, const int n)
             if (i != pivot_j)
             {
                 temp = m[i * n + pivot_j];
-                for (int j=0; j<n; j++) 
+                for (int j=0; j<n; j++)
                 {
                     m[i * n + j] -= m[pivot_j * n + j] * temp;
                 }
@@ -206,7 +207,7 @@ void solveMatrixEquation(double * m, double * rhs, const int n)
     delete[] reduced;
 }
 
-void fitPolynomial(double * x, double * y, const int n) 
+void fitPolynomial(double * x, double * y, const int n)
 {
     double * matrix = new double[n*n];
     for (int i=0; i<n; i++)
@@ -218,7 +219,8 @@ void fitPolynomial(double * x, double * y, const int n)
     }
 
     solveMatrixEquation(matrix, y, n);
-}    
+    delete[] matrix;
+}
 
 double calculatePolynomial(const double * coeffs, const int n, const double x)
 {
